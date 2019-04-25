e3b39b33a8017ec1d94a91dfd6599ffb36764d3b
wjymath <wjymath@foxmail.com>
2019-04-26 00:54:48 +0800

    logistic

---
+++ dont-over-fit/main.py
@@ -0,0 +1,34 @@
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+
+
+def load_train_data():
+    data = np.genfromtxt("train.csv", delimiter=',', skip_header=1)
+    return data
+
+
+def load_test_data():
+    data = np.genfromtxt("test.csv", delimiter=',', skip_header=1)
+    return data
+
+
+if __name__ == "__main__":
+    data = load_train_data()
+    test_data = load_test_data()
+    x = data[:, 2:]
+
+    y = data[:, 1]
+
+    test_x = test_data[:, 1:]
+    no = test_data[:, 0]
+    no = no.astype(int)
+    print(no[:3])
+    clf = LogisticRegression(random_state=0, solver='lbfgs',multi_class='multinomial').fit(x, y)
+    pre_y = clf.predict(test_x)
+    pre_y = pre_y.astype(int)
+    result = zip(no, pre_y)
+    file_out = open("submission.csv", 'w')
+    file_out.write('id,target\n')
+    for item in result:
+        file_out.write(str(item[0]) + ',' + str(item[1]) + "\n")
+    file_out.close()
