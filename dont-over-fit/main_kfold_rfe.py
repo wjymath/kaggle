1c7434c70a4883e70420c029360ac4a1aef0c988
wjymath <wjymath@foxmail.com>
2019-05-01 01:11:27 +0800

    logistic

--- dont-over-fit/main_kfold_rfe.py
+++ dont-over-fit/main_kfold_rfe.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import SGDClassifier
 from sklearn import svm
 from sklearn.model_selection import train_test_split
 from sklearn.feature_selection import RFE
@@ -29,10 +30,12 @@ if __name__ == "__main__":
 
     for i in range(10):
         x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.05)
-        func = LogisticRegression(random_state=0, solver='liblinear', class_weight='balanced', penalty='l1', warm_start=False)
+        func = LogisticRegression(random_state=0, C=2, solver='liblinear', class_weight='balanced', penalty='l1', warm_start=False)
+        # func = SGDClassifier(random_state=0, loss='log', class_weight='balanced', penalty='l1', l1_ratio=0.5, validation_fraction=0.5,
+        #                           warm_start=False, max_iter=1000, tol=1e-4)
         # func = svm.NuSVC(kernel='rbf', gamma='scale')
-        # func = svm.LinearSVC(penalty='l1', class_weight='balanced', dual=False, max_iter=10000)
-        clf = RFE(func, n_features_to_select=110)
+        # func = svm.LinearSVC(loss='hinge', class_weight='balanced', max_iter=10000)
+        clf = RFE(func, n_features_to_select=100)
         clf.fit(x_train, y_train)
         print(str(clf.score(x_test, y_test)))
         if clf.score(x_test, y_test) > 0.85:
