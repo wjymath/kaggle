4ed93f19a3955115ada6e84535cc938104f74579
wjymath <wjymath@foxmail.com>
2019-05-05 01:40:14 +0800

    提交loss function的正则项惩罚性

--- dont-over-fit/main.py
+++ dont-over-fit/main.py
@@ -24,8 +24,8 @@ if __name__ == "__main__":
     no = no.astype(int)
     print(no[:3])
     # 要求强正则性
-    clf = LogisticRegression(random_state=10, C=0.25, solver='liblinear', class_weight='balanced', penalty='l1', verbose=1).fit(x, y)
-    pre_y = clf.predict_proba(test_x)[:,1]
+    clf = LogisticRegression(random_state=10, C=0.15, solver='liblinear', class_weight='balanced', penalty='l1', verbose=1).fit(x, y)
+    pre_y = clf.predict_proba(test_x)[:, 1]
     # pre_y = pre_y.astype(int)
     result = zip(no, pre_y)
     file_out = open("submission.csv", 'w')
--- dont-over-fit/main_kfold_rfe.py
+++ dont-over-fit/main_kfold_rfe.py
@@ -4,6 +4,9 @@ from sklearn.linear_model import SGDClassifier
 from sklearn import svm
 from sklearn.model_selection import train_test_split
 from sklearn.feature_selection import RFE
+from sklearn.feature_selection import chi2
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import SelectFromModel
 
 def load_train_data():
     data = np.genfromtxt("train.csv", delimiter=',', skip_header=1)
@@ -30,19 +33,30 @@ if __name__ == "__main__":
 
     for i in range(10):
         x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.05)
-        func = LogisticRegression(random_state=0, C=2, solver='liblinear', class_weight='balanced', penalty='l1', warm_start=False)
+        func = LogisticRegression(random_state=10, solver='liblinear', class_weight='balanced', penalty='l1', warm_start=True)
         # func = SGDClassifier(random_state=0, loss='log', class_weight='balanced', penalty='l1', l1_ratio=0.5, validation_fraction=0.5,
         #                           warm_start=False, max_iter=1000, tol=1e-4)
         # func = svm.NuSVC(kernel='rbf', gamma='scale')
         # func = svm.LinearSVC(loss='hinge', class_weight='balanced', max_iter=10000)
-        clf = RFE(func, n_features_to_select=100)
-        clf.fit(x_train, y_train)
-        print(str(clf.score(x_test, y_test)))
-        if clf.score(x_test, y_test) > 0.85:
+        # x_new = SelectKBest(chi2, k=80).fit_transform(x_train, y_train)
+        sel_clf = SelectFromModel(func)
+        # clf = RFE(func, n_features_to_select=35)
+        sel_clf.fit(x_train, y_train)
+        x_new = sel_clf.transform(x_train)
+        print(x_new.shape)
+        new_test = sel_clf.transform(x_test)
+        # clf = LogisticRegression(random_state=0, C=0.35, solver='liblinear', class_weight='balanced', penalty='l1', warm_start=True).fit(x_new, y_train)
+        func = LogisticRegression(solver='liblinear', penalty='l1')
+        clf = RFE(func, n_features_to_select=35).fit(x_new, y_train)
+        print(str(clf.score(new_test, y_test)))
+        if clf.score(new_test, y_test) > 0.85:
             break
+    x = sel_clf.transform(x)
     print(str(clf.score(x, y)))
+
+    test_x = sel_clf.transform(test_x)
     pre_y = clf.predict(test_x)
-    pre_y = clf.predict_proba(test_x)[:,1]
+    pre_y = clf.predict_proba(test_x)[:, 1]
 
     result = zip(no, pre_y)
     file_out = open("submission.csv", 'w')
